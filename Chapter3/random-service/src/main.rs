c5b74c4996cb3d4572011b8764d70fee4950f631
Denis Kolodin <deniskolodin@gmail.com>
2018-07-24 06:57:50 +0300

    Add random-service to Chapter 3

---
+++ Chapter3/random-service/src/main.rs
@@ -0,0 +1,19 @@
+extern crate hyper;
+extern crate rand;
+
+use hyper::{Body, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 8080).into();
+    let builder = Server::bind(&addr);
+    let server = builder.serve(|| {
+        service_fn_ok(|_| {
+            let random_byte = rand::random::<u8>();
+            Response::new(Body::from(random_byte.to_string()))
+        })
+    });
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
+}
