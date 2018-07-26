7c8e49a5caf1a375451c6b4007be33a4e846862e
Denis Kolodin <deniskolodin@gmail.com>
2018-07-26 07:31:53 +0300

    Add example with config to Chapter 3

---
+++ Chapter3/random-service-with-config/src/main.rs
@@ -0,0 +1,86 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger as logger;
+extern crate dotenv;
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+extern crate hyper;
+extern crate rand;
+
+use std::env;
+use std::io::{self, Read};
+use std::fs::File;
+use std::net::SocketAddr;
+use dotenv::dotenv;
+use clap::{Arg, App};
+use hyper::{Body, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
+#[derive(Deserialize)]
+struct Config {
+    address: SocketAddr,
+}
+
+fn main() {
+    dotenv().ok();
+    logger::init();
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Rust Microservice")
+        .arg(Arg::with_name("config")
+             .short("c")
+             .long("config")
+             .value_name("FILE")
+             .help("Sets a custom config file")
+             .takes_value(true))
+        .arg(Arg::with_name("address")
+             .short("a")
+             .long("address")
+             .value_name("ADDRESS")
+             .help("Sets an address")
+             .takes_value(true))
+        .get_matches();
+    info!("Rand Microservice - v0.1.0");
+    trace!("Starting...");
+    let config = File::open("microservice.toml")
+        .and_then(|mut file| {
+            let mut buffer = String::new();
+            file.read_to_string(&mut buffer)?;
+            Ok(buffer)
+        })
+        .and_then(|buffer| {
+            toml::from_str::<Config>(&buffer)
+                .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+        })
+        .map_err(|err| {
+            warn!("Can't read config file: {}", err);
+        })
+        .ok();
+    let addr = matches.value_of("address")
+        .map(|s| s.to_owned())
+        .or(env::var("ADDRESS").ok())
+        .and_then(|addr| addr.parse().ok())
+        .or(config.map(|config| config.address))
+        .or_else(|| Some(([127, 0, 0, 1], 8080).into()))
+        .unwrap();
+    debug!("Trying to bind server to address: {}", addr);
+    let builder = Server::bind(&addr);
+    trace!("Creating service handler...");
+    let server = builder.serve(|| {
+        service_fn_ok(|req| {
+            trace!("Incoming request is: {:?}", req);
+            let random_byte = rand::random::<u8>();
+            debug!("Generated value is: {}", random_byte);
+            Response::new(Body::from(random_byte.to_string()))
+        })
+    });
+    info!("Used address: {}", server.local_addr());
+    let server = server.map_err(drop);
+    debug!("Run!");
+    hyper::rt::run(server);
+}
