70a13eda1d6ce658ab61647b562dbceec06f0d66
Denis Kolodin <deniskolodin@gmail.com>
2018-09-25 19:27:17 +0300

    Add static files to Example 11-2

--- Chapter11/example-11-2-routes/src/main.rs
+++ Chapter11/example-11-2-routes/src/main.rs
@@ -1,7 +1,8 @@
 extern crate actix_web;
 
 use std::cell::RefCell;
-use actix_web::{http, server, App, HttpRequest, Responder};
+use actix_web::{fs, server, App, HttpRequest, HttpResponse, Responder};
+use actix_web::http::{self, header, Method};
 
 struct AppState(RefCell<i64>);
 
@@ -21,21 +22,27 @@ fn count_down(req: HttpRequest<AppState>) -> impl Responder {
     format!("Count Down")
 }
 
-fn index(req: HttpRequest<AppState>) -> impl Responder {
-    format!("Rust Microservice (actix): {}", req.state().0.borrow())
+fn value(req: HttpRequest<AppState>) -> impl Responder {
+    format!("Counter: {}", req.state().0.borrow())
 }
 
 fn main() {
     server::new(|| {
         App::with_state(AppState::new())
-            .route("/", http::Method::GET, index)
             .scope("/count", |scope| {
                 scope.nested("/v1", |scope| {
                     scope
                         .route("/up", http::Method::GET, count_up)
                         .route("/down", http::Method::GET, count_down)
                 })
+                .route("/value", http::Method::GET, value)
             })
+            .handler("/static", fs::StaticFiles::new("static").unwrap())
+            .resource("/", |r| r.method(Method::GET).f(|_| {
+                HttpResponse::Found()
+                    .header(header::LOCATION, "static/index.html")
+                    .finish()
+}))
     })
     .bind("127.0.0.1:8080").unwrap().run();
 }
