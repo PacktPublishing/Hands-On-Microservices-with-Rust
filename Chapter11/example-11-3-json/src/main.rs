4d9e9949842825c8bd442f2c2cae468d0ada8ac8
Denis Kolodin <deniskolodin@gmail.com>
2018-10-09 23:24:19 +0300

    Add chapter 11

---
+++ Chapter11/example-11-3-json/src/main.rs
@@ -0,0 +1,60 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate actix_web;
+
+use std::cell::RefCell;
+use actix_web::{fs, server, App, Json, HttpRequest, HttpResponse, Responder};
+use actix_web::http::{self, header, Method};
+
+struct AppState(RefCell<i64>);
+
+#[derive(Deserialize)]
+struct NewState {
+    value: i64,
+}
+
+impl AppState {
+    fn new() -> Self {
+        AppState(RefCell::default())
+    }
+}
+
+fn count_up(req: HttpRequest<AppState>) -> impl Responder {
+    *req.state().0.borrow_mut() += 1;
+    format!("Count Up")
+}
+
+fn count_down(req: HttpRequest<AppState>) -> impl Responder {
+    *req.state().0.borrow_mut() -= 1;
+    format!("Count Down")
+}
+
+fn count_set((data, req): (Json<NewState>, HttpRequest<AppState>)) -> impl Responder {
+    *req.state().0.borrow_mut() = data.value;
+    format!("Count Set")
+}
+
+fn value(req: HttpRequest<AppState>) -> impl Responder {
+    format!("Counter: {}", req.state().0.borrow())
+}
+
+fn main() {
+    server::new(|| {
+        App::with_state(AppState::new())
+            .scope("/count", |scope| {
+                scope.nested("/v1", |scope| {
+                    scope
+                        .route("/up", http::Method::GET, count_up)
+                        .route("/down", http::Method::GET, count_down)
+                })
+                .route("/value", http::Method::GET, value)
+            })
+            .handler("/static", fs::StaticFiles::new("static").unwrap())
+            .resource("/", |r| r.method(Method::GET).f(|_| {
+                HttpResponse::Found()
+                    .header(header::LOCATION, "static/index.html")
+                    .finish()
+}))
+    })
+    .bind("127.0.0.1:8080").unwrap().run();
+}
