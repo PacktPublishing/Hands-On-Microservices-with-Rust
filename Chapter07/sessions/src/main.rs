cff4d806193e9b92a673bfbcd5b9a5778e7c8720
Denis Kolodin <deniskolodin@gmail.com>
2018-11-04 21:51:16 +0300

    Improve redis example

--- Chapter07/sessions/src/main.rs
+++ Chapter07/sessions/src/main.rs
@@ -5,10 +5,12 @@ use clap::{
     crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg, SubCommand,
 };
 use redis::{Client, Commands, Connection, RedisError};
+use std::collections::HashMap;
 
 const SESSIONS: &str = "sessions";
 const CMD_ADD: &str = "add";
 const CMD_REMOVE: &str = "remove";
+const CMD_LIST: &str = "list";
 
 fn add_session(conn: &Connection, token: &str, uid: &str) -> Result<(), RedisError> {
     conn.hset(SESSIONS, token, uid)
@@ -16,6 +18,10 @@ fn add_session(conn: &Connection, token: &str, uid: &str) -> Result<(), RedisErr
 
 fn remove_session(conn: &Connection, token: &str) -> Result<(), RedisError> {
     conn.hdel(SESSIONS, token)
+}
+
+fn list_sessions(conn: &Connection) -> Result<HashMap<String, String>, RedisError> {
+    conn.hgetall(SESSIONS)
 }
 
 fn main() -> Result<(), RedisError> {
@@ -33,8 +39,21 @@ fn main() -> Result<(), RedisError> {
             .help("Sets an address of db connection")
             .takes_value(true),
             )
-        .subcommand(SubCommand::with_name(CMD_ADD).about("add a session"))
-        .subcommand(SubCommand::with_name(CMD_REMOVE).about("remove a session"))
+        .subcommand(SubCommand::with_name(CMD_ADD).about("add a session")
+                    .arg(Arg::with_name("TOKEN")
+                         .help("Sets the token of a user")
+                         .required(true)
+                         .index(1))
+                    .arg(Arg::with_name("UID")
+                         .help("Sets the uid of a user")
+                         .required(true)
+                         .index(2)))
+        .subcommand(SubCommand::with_name(CMD_REMOVE).about("remove a session")
+                    .arg(Arg::with_name("TOKEN")
+                         .help("Sets the token of a user")
+                         .required(true)
+                         .index(1)))
+        .subcommand(SubCommand::with_name(CMD_LIST).about("print list of sesssions"))
         .get_matches();
 
     let addr = matches.value_of("database")
@@ -44,13 +63,20 @@ fn main() -> Result<(), RedisError> {
 
     match matches.subcommand() {
         (CMD_ADD, Some(matches)) => {
-            let token = matches.value_of("token").unwrap();
-            let uid = matches.value_of("uid").unwrap();
+            let token = matches.value_of("TOKEN").unwrap();
+            let uid = matches.value_of("UID").unwrap();
             add_session(&conn, token, uid)?;
         }
         (CMD_REMOVE, Some(matches)) => {
-            let token = matches.value_of("token").unwrap();
+            let token = matches.value_of("TOKEN").unwrap();
             remove_session(&conn, token)?;
+        }
+        (CMD_LIST, _) => {
+            println!("LIST");
+            let sessions = list_sessions(&conn)?;
+            for (token, uid) in sessions {
+                println!("Token: {:20}   Uid: {:20}", token, uid);
+            }
         }
         _ => {
             matches.usage(); // but unreachable
---
+++ Chapter07/sessions/test.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --name test-redis -p 6379:6379 redis
