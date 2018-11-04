176e0384e859f688919e0d49b0f642ed98b8a11f
Denis Kolodin <deniskolodin@gmail.com>
2018-11-04 21:31:15 +0300

    Improve example with database connections pool

--- Chapter07/users-pool/src/main.rs
+++ Chapter07/users-pool/src/main.rs
@@ -31,7 +31,19 @@ fn create_user(conn: &Connection, user: &User) -> Result<(), Error> {
         .map(drop)
 }
 
-#[derive(Deserialize)]
+fn list_users(conn: &Connection) -> Result<Vec<User>, Error> {
+    let res = conn.query("SELECT name, email FROM users", &[])?.into_iter()
+        .map(|row| {
+            User {
+                name: row.get(0),
+                email: row.get(1),
+            }
+        })
+        .collect();
+    Ok(res)
+}
+
+#[derive(Deserialize, Debug)]
 struct User {
     name: String,
     email: String,
@@ -39,6 +51,7 @@ struct User {
 
 const CMD_CRATE: &str = "create";
 const CMD_ADD: &str = "add";
+const CMD_LIST: &str = "list";
 const CMD_IMPORT: &str = "import";
 
 fn main() -> Result<(), failure::Error> {
@@ -57,12 +70,21 @@ fn main() -> Result<(), failure::Error> {
             .takes_value(true),
             )
         .subcommand(SubCommand::with_name(CMD_CRATE).about("create users table"))
-        .subcommand(SubCommand::with_name(CMD_ADD).about("add user to the table"))
+        .subcommand(SubCommand::with_name(CMD_ADD).about("add user to the table")
+                    .arg(Arg::with_name("NAME")
+                         .help("Sets the name of a user")
+                         .required(true)
+                         .index(1))
+                    .arg(Arg::with_name("EMAIL")
+                         .help("Sets the email of a user")
+                         .required(true)
+                         .index(2)))
+        .subcommand(SubCommand::with_name(CMD_LIST).about("print list of users"))
         .subcommand(SubCommand::with_name(CMD_IMPORT).about("import users from csv"))
         .get_matches();
 
     let addr = matches.value_of("database")
-        .unwrap_or("postgres://postgres@localhost:5433");
+        .unwrap_or("postgres://postgres@localhost:5432");
     let manager = PostgresConnectionManager::new(addr, TlsMode::None)?;
     let pool = r2d2::Pool::new(manager)?;
     let conn = pool.get()?;
@@ -72,10 +94,16 @@ fn main() -> Result<(), failure::Error> {
             create_table(&conn)?;
         }
         (CMD_ADD, Some(matches)) => {
-            let name = matches.value_of("name").unwrap().to_owned();
-            let email = matches.value_of("email").unwrap().to_owned();
+            let name = matches.value_of("NAME").unwrap().to_owned();
+            let email = matches.value_of("EMAIL").unwrap().to_owned();
             let user = User { name, email };
             create_user(&conn, &user)?;
+        }
+        (CMD_LIST, _) => {
+            let list = list_users(&conn)?;
+            for user in list {
+                println!("Name: {:20}    Email: {:20}", user.name, user.email);
+            }
         }
         (CMD_IMPORT, _) => {
             let mut rdr = csv::Reader::from_reader(io::stdin());
