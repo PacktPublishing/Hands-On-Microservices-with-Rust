13af0ae1d44c1755d1b62e507be743fa82743b2a
Denis Kolodin <deniskolodin@gmail.com>
2018-11-25 15:48:48 +0300

    Add tests for mailer

--- Chapter13/integration/tests/mails.rs
+++ Chapter13/integration/tests/mails.rs
@@ -1,8 +1,26 @@
-use reqwest::{self, StatusCode};
-
 mod utils;
+
+use reqwest::{self, StatusCode};
+use self::utils::Method;
+
+const URL: &str = "http://localhost:8002";
+
+fn url(path: &str) -> String {
+    URL.to_owned() + path
+}
 
 #[test]
 fn mails_healthcheck() {
-    utils::healthcheck("http://localhost:8002/", "Mailer Microservice");
+    utils::healthcheck(&url("/"), "Mailer Microservice");
+}
+
+#[test]
+fn send_mail() {
+    let email = utils::rand_str() + "@example.com";
+    let code = utils::rand_str();
+    let params = vec![
+        ("to", email.as_ref()),
+        ("code", code.as_ref()),
+    ];
+    let _: () = utils::request(Method::POST, &url("/send"), params);
 }
