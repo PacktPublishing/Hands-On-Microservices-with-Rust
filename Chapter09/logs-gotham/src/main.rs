49179698b664a4cb20b6017c6006d023e2cbc2d6
Denis Kolodin <deniskolodin@gmail.com>
2019-01-10 11:05:47 +0300

    Add async handler to gotham example

--- Chapter09/logs-gotham/src/main.rs
+++ Chapter09/logs-gotham/src/main.rs
@@ -6,21 +6,40 @@ extern crate mime;
 
 use failure::{Error, format_err};
 use futures::{Future, Stream};
+use futures::future;
+use gotham::handler::{IntoHandlerFuture, HandlerFuture};
+use gotham::middleware::state::StateMiddleware;
+use gotham::pipeline::single::single_pipeline;
+use gotham::pipeline::single_middleware;
+use gotham::router::Router;
+use gotham::router::builder::{DefineSingleRoute, DrawRoutes, build_router};
 use gotham::state::{FromState, State};
+use std::sync::{Arc, Mutex};
 use tokio::net::TcpStream;
 use tokio::runtime::Runtime;
 use tokio_postgres::{Client, Connection, NoTls};
 
 const HELLO_WORLD: &'static str = "Hello World!";
 
-#[derive(StateData, Eq, PartialEq, Debug)]
+#[derive(Clone, StateData)]
 struct ConnState {
+    client: Arc<Mutex<Client>>,
 }
 
+impl ConnState {
+    fn new(client: Client) -> Self {
+        Self {
+            client: Arc::new(Mutex::new(client)),
+        }
+    }
+}
+
+/*
 pub fn say_hello(state: State) -> (State, &'static str) {
     //let conn = ConnState::borrow_from(&state);
     (state, HELLO_WORLD)
 }
+*/
 
 fn connect(
     s: &str,
@@ -29,6 +48,21 @@ fn connect(
     TcpStream::connect(&"127.0.0.1:5432".parse().unwrap())
         .map_err(|e| panic!("{}", e))
         .and_then(move |s| builder.connect_raw(s, NoTls))
+}
+
+fn say_hello(mut state: State) -> Box<HandlerFuture> {
+    let conn = ConnState::borrow_from(&state);
+    let fut = (state, HELLO_WORLD).into_handler_future();
+    Box::new(fut)
+}
+
+fn router(state: ConnState) -> Router {
+    let middleware = StateMiddleware::new(state);
+    let pipeline = single_middleware(middleware);
+    let (chain, pipelines) = single_pipeline(pipeline);
+    build_router(chain, pipelines, |route| {
+        route.get("/").to(say_hello);
+    })
 }
 
 pub fn main() -> Result<(), Error> {
@@ -40,16 +74,18 @@ pub fn main() -> Result<(), Error> {
 
     let prepare = client.prepare("SELECT 1::INT4");
     let statement = runtime.block_on(prepare).unwrap();
-    println!("HERE!");
     let select = client.query(&statement, &[]).collect().map(|rows| {
         println!("{:?}", rows[0].get::<_, i32>(0));
     });
     let res = runtime.block_on(select).unwrap();
     println!("{:?}", res);
 
+    let state = ConnState::new(client);
+    let router = router(state);
+
     let addr = "127.0.0.1:7878";
     println!("Listening for requests at http://{}", addr);
-    gotham::start_on_executor(addr, || Ok(say_hello), runtime.executor());
+    gotham::start_on_executor(addr, router, runtime.executor());
     runtime
         .shutdown_on_idle()
         .wait()
