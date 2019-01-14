53ae8cebe13e7e01b773ce23aace1198abbc9042
Denis Kolodin <deniskolodin@gmail.com>
2019-01-14 07:29:41 +0300

    Add JSON-RPC example

---
+++ Chapter06/jsonrpc-ring/second.sh
@@ -0,0 +1,1 @@
+RUST_LOG=jsonrpc_ring=trace RUST_BACKTRACE=1 ADDRESS=127.0.0.1:5555 NEXT=127.0.0.1:4444 cargo watch -x run
---
+++ Chapter06/jsonrpc-ring/test.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" --data-binary '{"jsonrpc":"2.0","id":"curl","method":"start_roll_call","params":[]}' http://127.0.0.1:4444
