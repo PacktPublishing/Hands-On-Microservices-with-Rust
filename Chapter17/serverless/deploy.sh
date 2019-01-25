95f65a5c239e0aae008d68b3bd58aa0ce90891f6
Denis Kolodin <deniskolodin@gmail.com>
2019-01-25 23:51:17 +0300

    Add lambda deployment script

--- Chapter17/serverless/deploy.sh
+++ Chapter17/serverless/deploy.sh
@@ -1,3 +1,56 @@
-#sls deploy
-#sls client deploy
-aws s3 cp ./config.js s3://rust-sls-aws/js/config.js
+set -e
+
+extract() {
+    echo "$DATA" | grep $1 | cut -d " " -f2
+}
+
+deploy() {
+    sls deploy
+    sls client deploy
+    DATA=`sls info -v`
+    POOL_ID=`extract PoolId`
+    POOL_CLIENT_ID=`extract PoolClientId`
+    REGION=`extract region`
+    ENDPOINT=`extract ServiceEndpoint`
+
+    CONFIG="
+    window._config = {
+        cognito: {
+            userPoolId: '$POOL_ID',
+            userPoolClientId: '$POOL_CLIENT_ID',
+            region: '$REGION'
+        },
+        api: {
+            invokeUrl: '$ENDPOINT'
+        }
+    };
+    "
+
+    echo "$CONFIG" | aws s3 cp - s3://rust-sls-aws/js/config.js
+    INDEX=`extract BucketURL`
+    echo "INDEX: $INDEX"
+}
+
+update() {
+    sls deploy
+}
+
+remove() {
+    sls client remove
+    sls remove
+}
+
+case $1 in
+    "deploy")
+        deploy
+        ;;
+    "update")
+        update
+        ;;
+    "remove")
+        remove
+        ;;
+    *)
+        echo "Available subcommands: deploy | remove"
+        ;;
+esac
--- Chapter17/serverless/lambda_1/src/main.rs
+++ Chapter17/serverless/lambda_1/src/main.rs
@@ -1,30 +1,85 @@
 use lambda_runtime::{error::HandlerError, lambda, Context};
+use log::debug;
+use serde_derive::{Serialize, Deserialize};
 use serde_json::Value;
+use std::collections::HashMap;
+use std::error::Error;
 
-fn main() {
-    lambda!(handler)
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug)?;
+    debug!("TEST");
+    lambda!(handler);
+    Ok(())
 }
 
-fn handler(
-    event: Value,
-    _: Context,
-) -> Result<Value, HandlerError> {
-    Ok(event)
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Location {
+    latitude: f64,
+    longitude: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use serde_json::json;
-
-    #[test]
-    fn handler_handles() {
-        let event = json!({
-            "answer": 42
-        });
-        assert_eq!(
-            handler(event.clone(), Context::default()).expect("expected Ok(_) value"),
-            event
-        )
-    }
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Request {
+    body: RequestBody,
 }
+
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct RequestBody {
+    pickup_location: Location,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Unicorn {
+    name: String,
+    color: String,
+    gender: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct ResponseBody {
+    ride_id: String,
+    unicorn: Unicorn,
+    unicorn_name: String,
+    eta: String,
+    rider: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Response {
+    body: String,
+    status_code: u16,
+    headers: HashMap<String, String>,
+}
+
+fn handler(event: Value, _: Context) -> Result<Response, HandlerError> {
+    debug!("EVENT: {:?}", event);
+    //let request = serde_json::from_value(&event).unwrap();
+    let unicorn = Unicorn {
+        name: "Bucephalus".into(),
+        color: "Golden".into(),
+        gender: "Male".into(),
+    };
+    let body = ResponseBody {
+        ride_id: "ride-unique-id".into(),
+        unicorn,
+        unicorn_name: "Bucephalus".into(),
+        eta: "30 seconds".into(),
+        rider: "congnito-here".into(),
+    };
+    let mut headers = HashMap::new();
+    headers.insert("Access-Control-Allow-Origin".into(), "*".into());
+    let body = serde_json::to_string(&body).unwrap();
+    let resp = Response {
+        status_code: 201,
+        body,
+        headers,
+    };
+    Ok(resp)
+}
+
